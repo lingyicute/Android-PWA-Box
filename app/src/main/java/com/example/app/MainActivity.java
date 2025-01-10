5a5b7e8c0230d94a85598d3774128ef15304e9c2
lingyicute <160479206+lingyicute@users.noreply.github.com>
2025-01-10 23:16:12 +0800

    update

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.lyi.cloud;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -77,10 +77,10 @@ public class MainActivity extends Activity {
         });
 
         // REMOTE RESOURCE
-        mWebView.loadUrl("https://example.com");
+        mWebView.loadUrl("https://c.92li.us.kg");
 
         // LOCAL RESOURCE
-        mWebView.loadUrl("file:///android_asset/index.html");
+        // mWebView.loadUrl("file:///android_asset/index.html");
     }
 
     @Override
